9afd9ee7166461a6dd9af0f46ec8c27b6e23fcfc
Vinces <vince.egos@gmail.com>
2021-11-03 05:23:49 +0100

    fix (env) improve env config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { join, resolve } from "path";
+import { join } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "./",
   resolve: {
     alias: {
       "@": join(__dirname, "src"),
